896d4b160b33c0df544c812904657724c30b9eaa
Lori Henderson <lorim95@gmail.com>
2020-10-15 23:33:27 -0400

    Couldn't remember how to do it properly so just focused on mimicking the look

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const topColors = [
+      {left: "blue", middle: "red", right: "orange"},
+    ]
+    const middleColors = [
+      {mleft: "yellow", mmiddle: "green", mright: "purple"},
+    ]
+    const bottomColors = [
+      {bleft: "lightblue", bmiddle: "pink", bright: "silver"},
+    ]
+
+    const renderColors = (color) => {
+      return(
+        <div>
+        <tr id="top">
+          <td id="tleft">{color.left}</td>
+          <td id="tmiddle">{color.middle}</td>
+          <td id="tright">{color.right}</td>
+        </tr>
+        <br></br>
+        <tr id="middle">
+          <td id="mleft">{color.mleft}</td>
+          <td id="mmiddle">{color.mmiddle}</td>
+          <td id="mright">{color.mright}</td>
+        </tr>
+        <br></br>
+        <tr id="bottom">
+          <td id="bleft">{color.bleft}</td>
+          <td id="bmiddle">{color.bmiddle}</td>
+          <td id="bright">{color.bright}</td>
+        </tr>
+        <br></br>
+        </div>
+        )
+    }
+
+    return (
+      <div>
+      <tbody>{topColors.map(renderColors)}</tbody>
+      <tbody>{middleColors.map(renderColors)}</tbody>
+      <tbody>{bottomColors.map(renderColors)}</tbody>
+      </div>
+    );
+  }
 }
 
 export default App;
